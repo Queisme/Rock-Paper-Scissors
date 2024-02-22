eb7c0fdb4de4de9c2a2ac4e9402e2bb13bc110b5
Queisme <queiskey@gmail.com>
2024-02-22 02:17:11 -0600

    added and moved commits - smoothed out one loop

--- javascript.js
+++ javascript.js
@@ -6,11 +6,41 @@ begin with a function called 'getComputerChoice' that will randomly return eithe
 2. find out the mathematical formula that will allow the computer to randomly pick between the three
 3. implement and return
 */
-  
+/* 
+write a function that plays a single round of RPS. It should take two parameters - the 'playerSelection' and 'computerSelection' - then return a string that declares the winner or tie of the round: "You Lost! Paper beats Rock"
+**Make 'playerSelection' case-insensitive 
 
-let choices = ['rock', 'paper', 'scissors'];
+1. need to take into in from player.
+**make case-insensitive
+2. compare playerSelection to getComputerChoice.
+3. if tie respond with It's a tie
+4. compare the two through all of the possibilities and declare winner
+paper-rock-scissors or loser and why.
+*/
+/*
+new function called 'playGame()' use previous function inside it to play a 5 round game that keeps score and reports a winner or loser at the end
 
-function getComputerChoice() {
-    console.log(choices[(Math.floor(Math.random() * choices.length))]);
+1. Make a loop
+2. learn how to tell computer to keep score and report winner and loser results after each iteration
+3. put working 'getComputerChoice' function inside loop function
+*/
+
+function getComputerChoice(playerSelection) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerSelection = (choices[(Math.floor(Math.random() * choices.length))]);
+            
+    if (computerSelection === playerSelection) {
+        return 'It\'s a tie. Try again!';
+    } else if (
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+              ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}. `;
+            }
 }
-getComputerChoice()
+const playerSelection = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+
+console.log(getComputerChoice(playerSelection));
